bd90d95c096132b32fbe8acb3a28a08854c334d5
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-01-08 22:58:25 -0300

    Test example

--- backend/src/pages/tests.py
+++ backend/src/pages/tests.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
 
 
 class PageTest(TestCase):
 
-    def test_fail(self):
-        self.assertEqual(1, 3)
+    def test_example(self):
+        self.assertEqual(2, 2)
